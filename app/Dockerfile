9f9af0f57153ecf53642cdc7ee71be0555b1a127
Shashank Barsin <shashankbarsin@gmail.com>
2019-05-03 08:41:39 +0530

    Add sample to be used in container template docs

---
+++ app/Dockerfile
@@ -0,0 +1,11 @@
+FROM node:8
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm ci --only=production
+
+COPY . .
+
+EXPOSE 8080
+CMD [ "npm", "start" ]
---
+++ app/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
