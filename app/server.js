56d46a3917d0b36656cb7413d909a5ff5420ab76
Troy McElwee <tmcel1118@cbs.com>
2020-01-27 16:15:59 -0500

    try again

--- app/server.js
+++ app/server.js
@@ -6,21 +6,11 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 
 const app = express();
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('file not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
+app.register('.html', require('jade'));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
