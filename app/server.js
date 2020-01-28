297277c5c6637406987c948cddc120243db23955
Troy McElwee <tmcel1118@cbs.com>
2020-01-28 11:21:16 -0500

    server js update

--- app/server.js
+++ app/server.js
@@ -7,16 +7,28 @@ const hostname = '0.0.0.0';
 const port = 80;
 
 // Render page
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    const server = http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port, hostname, () => {
-    	// Console
-	  	console.log(`Server running at http://${hostname}:${port}/`);
-	});
-});
+http.createServer(function(req, res){
+
+    if(req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
+
