6755ad24f9b1c609e3c72d40eeb43c5afd8a4093
nimai-agarwal <nimai.agar@gmail.com>
2020-05-19 14:22:52 -0400

    added python script

---
+++ ttdata_script.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == "__main__":
+    manifestData = sys.argv[1]
+    deployer = sys.argv[2]
+    manifest = manifestData.split()
+    digest = manifest[0]
+    creationDate = manifest[2]
+    print(digest)
+    print(creationDate)
