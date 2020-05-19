867dfa6ce02c991fce831e0e18e4cfb34e4e6141
Nimai Agarwal <47481810+nimai-agarwal@users.noreply.github.com>
2020-05-19 14:46:33 -0400

    Update ttdata_script.py

--- ttdata_script.py
+++ ttdata_script.py
@@ -2,9 +2,10 @@ import sys
 
 if __name__ == "__main__":
     manifestData = sys.argv[1]
+    print(manifestData)
     deployer = sys.argv[2]
     manifest = manifestData.split()
     digest = manifest[0]
-    creationDate = manifest[1]
+    creationDate = manifest[2]
     print(digest)
     print(creationDate)
