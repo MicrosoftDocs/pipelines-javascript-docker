6e5d4a6b19d67ca41651f83e56d546c249900820
Nimai Agarwal <47481810+nimai-agarwal@users.noreply.github.com>
2020-05-19 15:03:50 -0400

    Update ttdata_script.py

--- ttdata_script.py
+++ ttdata_script.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     digest = sys.argv[1]
     creationDate = sys.argv[3]
     deployer = sys.argv[4]
+    imageName = sys.arv[5]
     print(digest)
     print(creationDate)
     print(deployer)
+    print(imageName)
