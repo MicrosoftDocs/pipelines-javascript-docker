57ab72d25daf49314c177ff95714d3eed09a3720
pradeepiyerust <pradeepiyerust@users.noreply.github.com>
2022-03-14 15:35:29 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world - Testing\n');
+  res.send('Hello world - Testing from feature\n');
 });
 
 app.listen(PORT, HOST);
