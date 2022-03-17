fcab13c50417acccb72a34179684f5fd7204b91d
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2022-03-17 13:01:30 -0400

    Updating express app and Dockerfile

--- app/server.js
+++ app/server.js
@@ -1,14 +1,11 @@
-'use strict';
+const express = require('express')
+const app = express()
+const port = 3000
 
-const express = require('express');
-
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+  res.send('Hello World!')
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
