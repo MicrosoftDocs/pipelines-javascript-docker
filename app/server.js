715a49c3c62ffc20f72fc86622ba3ae236bc2e1a
Philip Belder <philipb@avanan.com>
2022-04-11 10:09:57 +0300

    added fibo

--- app/server.js
+++ app/server.js
@@ -5,9 +5,20 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+function fibo(n) { 
+
+  console.log("N is:" + n)
+  if (n < 2)
+      return 1;
+  else   return fibo(n - 2) + fibo(n - 1);
+}
+
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  let num = parseInt(req.headers.fibo); 
+  console.log(num)
+  res.send('Hello world ' + num);
 });
 
 app.listen(PORT, HOST);
